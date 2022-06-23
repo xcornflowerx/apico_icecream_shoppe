88ee12c593952f48fe87b0fe64098cc1ffda2cff
Cornflower <50751101+xcornflowerx@users.noreply.github.com>
2022-06-23 09:02:55 -0400

    APICO Ice Cream Shoppe Mod
    Initial release introduces:
    - a new NPC
    - a new flower (flower131)
    - 11 ice creams
    - 3 new items for 3 secret ice cream flavours that
    the player discovers by chance
    
    Signed-off-by: Cornflower <50751101+xcornflowerx@users.noreply.github.com>

--- mod.lua
+++ mod.lua
@@ -1,24 +1,34 @@
--- cornflower's ice cream shoppe mod
--- cornflower hex code #6495ed, rgb = (100,149,237)
-
 MOD_NAME = "icecream_shoppe"
+KNOWN_SECRET_FLAVOURS_TABLE = {}
 
 function register()
 	return {
 		name = MOD_NAME,
-		hooks = {"ready"},
-		modules = {"cornflower", "icecream_dishes", "icecream_maker"}
+		hooks = {},
+		modules = {"cornflower", "cornflower_flower", "icecream_dishes", "icecream_maker"}
 	}
 end
 
-function ready()
-	-- this is where we would set things up if we had anything to set up :)
-end
-
 function init()
-	define_cornflower()
+	-- load user data (used for determining if the secret ice cream flavours have been discovered yet)
+	api_get_data()
+
+	-- other define calls for the ice cream shoppe
+	define_cornflower() -- the npc, not the flower
+	define_cornflower_flower() -- :)
 	define_iceream_dishes()
 	define_icecream_maker()
+
 	return "Success"
 end
 
+function data(ev, data)
+	if ev == "LOAD" and data ~= nil then
+		KNOWN_SECRET_FLAVOURS_TABLE = data["known_secret_flavours"]
+		override_icecream_shoppe_stock(get_cornflower(), KNOWN_SECRET_FLAVOURS_TABLE)
+	elseif ev == "SAVE" then
+		if data == nil then
+			api_log("data", "failed to update our data.json")
+		end
+	end
+end
